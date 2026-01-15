d63aa47bea7c9eb57c8e87ef4267ae4745cc2093
Swapnanil Chakraborty <swapnanilc729@gmail.com>
2026-01-15 16:35:17 +0530

    Add files via upload

---
+++ func.js
@@ -0,0 +1,8 @@
+function removeneelu(){
+    let target=document.querySelector('#member-row:nth-of-type(2)');
+    if(target){
+        target.remove();
+        alert("neelkantha is kicked out");
+        
+    }
+}
